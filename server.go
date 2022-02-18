886f1068e2b05fe18edcb480a6f7151622892482
rsmith-github <rsmith.git@gmail.com>
2022-02-18 22:41:19 +0000

    update

--- server.go
+++ server.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Starting server at port 8080\n")
+	// file Server to host static files
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server at http://localhost:8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Holds all the logic related to the /hello request
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+// handler to accept the /form request
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	input := r.FormValue("input")
+
+	fmt.Fprintf(w, "Input = %s\n", input)
 }
